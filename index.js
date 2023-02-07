87ee6b9144d6cc4b8d576c8dfb314090dde5e8e6
mohitverse <mohit@prabidhilabs.com>
2023-02-07 16:46:19 +0545

    Added :: Frontend

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 
+const path = require('path');
+
 const express = require('express');
 
 const port = process.env.PORT || 8000;
@@ -9,6 +11,9 @@ const app = express();
 // Enable body parse
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/openai', require('./routes/openaiRoutes'));
 
